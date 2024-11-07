c10e7be97cb1c9e07954f2a7afdaead037da1c6b
RAHUL TIWARI <97533449+RahulCodeTiwari@users.noreply.github.com>
2024-11-07 16:46:24 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,5 +6,6 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    {/* You can include text or other elements after App */}
   </React.StrictMode>,
 )
